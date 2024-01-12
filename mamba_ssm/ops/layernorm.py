7d99a993b1e781f88923dbcaa465ac5c0f5ab8e9
Bernardo Ramos <berna.gensis@gmail.com>
2024-01-12 00:52:23 -0300

    simplify rms_norm

--- mamba_ssm/ops/layernorm.py
+++ mamba_ssm/ops/layernorm.py
@@ -11,19 +11,7 @@ class RMSNorm(torch.nn.Module):
         super().__init__()
         self.eps = eps
         self.weight = torch.nn.Parameter(torch.empty(hidden_size, **factory_kwargs))
-        self.register_parameter("bias", None)
-        self.reset_parameters()
 
-    def reset_parameters(self):
-        torch.nn.init.ones_(self.weight)
-
-    def forward(self, x, residual=None):
-        weight = self.weight
-        bias = self.bias
-        eps = self.eps
-        if residual is not None:
-            x = (x + residual).to(x.dtype)
-        rstd = 1 / torch.sqrt((x.square()).mean(dim=-1, keepdim=True) + eps)
-        out = (x * rstd * weight) + bias if bias is not None else (x * rstd * weight)
-        out = out.to(x.dtype)
-        return out
+    def forward(self, x):
+        rstd = 1 / torch.sqrt(x.square().mean(dim=-1, keepdim=True) + self.eps)
+        return x * rstd * self.weight
